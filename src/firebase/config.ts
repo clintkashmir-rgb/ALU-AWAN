2f8116e91795046077c04c882786f3cad6d2504d
clintkashmir15 <clintkashmir@gmail.com>
2026-02-28 06:46:55 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-project.firebaseapp.com",
-  projectId: "placeholder-project",
-  storageBucket: "placeholder-project.appspot.com",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id"
+  "projectId": "studio-5816171861-9dff2",
+  "appId": "1:110058352326:web:d4063ea98c63896b8271c7",
+  "apiKey": "AIzaSyCRVUbtJ-A-TaONipJ-PBqFRYj5i7nBQAM",
+  "authDomain": "studio-5816171861-9dff2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110058352326"
 };
